d07fe117c51d5dec29a217c9ce75d690c98af1ff
jmdiazpu <jmdiazpu@outlook.com>
2025-10-26 21:38:21 -0500

    Add countries entity

---
+++ gcam/gcam.Backend/Data/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using gcam.Shared.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace gcam.Backend.Data;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Country> Countries { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<Country>().HasIndex(x => x.Name).IsUnique();
+    }
+}
---
+++ gcam/gcam.Shared/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gcam.Shared.Entities;
+
+public class Country
+{
+    public int Id { get; set; }
+
+    [Display(Name = "País")]
+    [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+    [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+    public string Name { get; set; } = null!;
+}
