97e29e6f3b4b11b8e157244d8ee190c697fd82f9
jmdiazpu <jmdiazpu@outlook.com>
2025-10-28 20:06:16 -0500

    Add Db seeder

---
+++ gcam/gcam.Backend/Data/SeedDb.cs
@@ -0,0 +1,28 @@
+﻿using gcam.Shared.Entities;
+
+namespace gcam.Backend.Data;
+
+public class SeedDb
+{
+    private readonly DataContext _context;
+
+    public SeedDb(DataContext context)
+    {
+        _context = context;
+    }
+
+    public async Task SeedDbAsync()
+    {
+        await _context.Database.EnsureCreatedAsync();
+        await CheckCountriesAsync();
+    }
+
+    private async Task CheckCountriesAsync()
+    {
+        if (!_context.Countries.Any())
+        {
+            _context.Countries.Add(new Country { Name = "Colombia" });
+            await _context.SaveChangesAsync();
+        }
+    }
+}
